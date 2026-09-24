437b4df3670ebf9fbf447cf7dfba7bbc4cc479d0
Muhammad Ibrahim <Ibrahimgp@gmail.com>
2026-09-24 11:28:30 +0200

    setting up the staging part for dbt

--- models/staging/staging_airports.sql
+++ models/staging/staging_airports.sql
@@ -1,17 +1,20 @@
 -- Active: 1790165417836@@data-analytics-course-2.c8g8r1deus2v.eu-central-1.rds.amazonaws.com@5432@nf_da_onl_en_200726@s_muhammadibrahim
 
-version: 2
-sources:
-
-- name: weather_data
-  schema: c_teacher
-  tables:
-    - name: weather_daily_raw
-    - name: weather_hourly_raw
-
-- name: flights_data
-  schema: public
-  tables:
-    - name: flights
-    - name: airports
+WITH airports_regions_join AS (
+        SELECT
+                a.faa,
+                a.name,
+                a.city,
+                a.country,
+                r.region,
+                a.lat,
+                a.lon,
+                a.alt,
+                a.tz,
+                a.dst
+        FROM {{source('flights_data', 'airports')}} AS a
+        LEFT JOIN {{source('flights_data', 'regions')}} AS r
+        USING (country)
+    )
+    SELECT * FROM airports_regions_join
     - name: regions
